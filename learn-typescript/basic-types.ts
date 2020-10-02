4e66d6e0b6403941b93b8d31c86755c3adb42517
lyctea <396136337@qq.com>
2020-10-02 10:04:05 +0800

    feat: 联合类型

--- learn-typescript/basic-types.ts
+++ learn-typescript/basic-types.ts
@@ -11,3 +11,17 @@ let n: null = null
 
 // undefined、null是所有类型的子类型， undefined类型的变量可以赋值给number类型
 let num: number = undefined
+
+
+let notSure: any = 4
+notSure = 'maybe it is a stirng'
+notSure = true
+
+notSure.myName
+notSure.getName()
+
+// 联合类型
+let  numberOrString: number | string
+numberOrString = 123
+numberOrString = '123'
+// numberOrString = true error
