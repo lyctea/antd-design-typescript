019adb27f6f181ceb58e9193c146ae5068ce9dff
lyctea <396136337@qq.com>
2020-10-02 09:59:04 +0800

    feat: 基础类型

---
+++ learn-typescript/basic-types.ts
@@ -0,0 +1,13 @@
+let isDone: boolean = false
+
+let age: number = 20
+let binaryNumbber: number = 0b1111
+
+let firstName: string = 'jupiter'
+let message: string = `Hello, ${firstName}, age is ${age}`
+
+let u: undefined = undefined
+let n: null = null
+
+// undefined、null是所有类型的子类型， undefined类型的变量可以赋值给number类型
+let num: number = undefined
