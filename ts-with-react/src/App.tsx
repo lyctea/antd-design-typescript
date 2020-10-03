3182a267674b38886e071fd67566e64b99083a31
lyctea <396136337@qq.com>
2020-10-03 11:14:26 +0800

    feat: hello 组件

--- ts-with-react/src/App.tsx
+++ ts-with-react/src/App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+import Hello from './Hello'
+
 function App() {
   return (
     <div className="App">
@@ -10,6 +13,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Hello /> 
         <a
           className="App-link"
           href="https://reactjs.org"
